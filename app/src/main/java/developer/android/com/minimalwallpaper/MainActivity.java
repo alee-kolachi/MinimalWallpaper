223605e5d8376a808564fd94461a835776daf065
Alee <alee.kolachi@gmail.com>
2019-07-15 17:24:23 +0500

    Menu items added. Now will be working on GridView onwards.

--- app/src/main/java/developer/android/com/minimalwallpaper/MainActivity.java
+++ app/src/main/java/developer/android/com/minimalwallpaper/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,5 +19,26 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main_menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.save:{
+
+            }
+                return true;
+            case R.id.download:{
+
+            }
+                return true;
+            case R.id.share:{
+
+            }
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
     }
 }
